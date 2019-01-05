7526b32db6391915d52e1ed659e1288d85813c03
snehagit04 <46393689+snehagit04@users.noreply.github.com>
2019-01-05 13:04:39 +0530

    Update Bird.java
    Modified the print statement

--- CoreJava/src/pond/shore/Bird.java
+++ CoreJava/src/pond/shore/Bird.java
@@ -3,6 +3,6 @@ public class Bird {
 	protected String text = "floating"; // protected access
 	protected void floatInWater() { // protected access
 	System.out.println(text);
-	System.out.println("hello");
+	System.out.println("hello , this is a bird class");
 }
 }
